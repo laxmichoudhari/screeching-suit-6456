4d8d6e4a3d1c82818d202e86d5962e8f01f2b116
laxmi choudhari <laxmichoudhari04@gmail.com>
2023-01-19 11:55:35 +0530

    day 2 work added

---
+++ src/components/Constants.js
@@ -0,0 +1,271 @@
+export const FeaturedBrands = [
+    {
+      title: "Baidyanath(Nagpur)",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/d6f04684-ecaf-454b-a11e-360abf4ff603.jpeg",
+    },
+    {
+      title: "Veet",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/xo0ots8fv2ylazjd2ovb.png",
+    },
+    {
+      title: "Nivea",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/dbcf099c-fcc2-4c84-abd9-df54e069364c.png",
+    },
+    {
+      title: "Contour",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/49228ade-73dc-420e-8c66-5a9e4c521b2b.png",
+    },
+    {
+      title: "Hansaplast",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/45215aa0-e0a5-40ef-9a0c-e46103e4a371.png",
+    },
+    {
+      title: "Optimum Nutrition",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/52abeeb0-84c6-48ae-85f0-2c409cb11a84.jpg",
+    },
+    {
+      title: "Durex",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/x4t7t0lyryd2cx9qsr0j.png",
+    },
+    {
+      title: "Organic India",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/b62gq72fszjy0vqkavsq.png",
+    },
+    {
+      title: "Stayfree",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/539a1389-b988-43fb-b8c7-d7ecd926144e.png",
+    },
+    {
+      title: "Dr vaidyad's",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/jk8ruwbfjbg17qteedfc.png",
+    },
+  ];
+  
+  export const popularCats = [
+    {
+      title: "Diabetes Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/h8qo5dk91lliwwihbwnf.png",
+    },
+    {
+      title: "Nutrition Drinks",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/uq1gwxslqqrxxwv6zi6j.jpg",
+    },
+    {
+      title: "Healthcare Devices",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/fbigjgxv2wquqqb3galn.png",
+    },
+    {
+      title: "Homeopathy",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/j7ruar88gacbzksuadjn.png",
+    },
+    {
+      title: "Sexual Wellness",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/p1blaka183nc1zlil8tq.png",
+    },
+    {
+      title: "Skin Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/bkhusfgfiu9qedowph2i.png",
+    },
+    {
+      title: "Vitamins & Supplements",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/cabpuknnlyyzgqfnb4x5.png",
+    },
+    {
+      title: "Pain Relief",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/au6x9ngzsixzfzae9pyv.png",
+    },
+    {
+      title: "Ayurveda",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/x2z8u1osqutmxvx56hlw.png",
+    },
+  ];
+  
+  export const health = [
+    {
+      title: "Diabetes Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/c2a0598f-483c-48ff-9783-71e402aa28d3.png",
+    },
+    {
+      title: "Cardiac Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ab1da5f4-c074-47d2-b278-a5fbd2c93f1f.png",
+    },
+    {
+      title: "Stomach Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/18e90e64-9e8c-47c5-ae33-508903429450.png",
+    },
+    {
+      title: "Livar Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/14f0bebe-6220-4ac7-b4bb-c5b425975af3.png",
+    },
+    {
+      title: "Kidney Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/555f6a24-3e20-41ee-a03b-e3dbda183392.png",
+    },
+    {
+      title: " Bone Joints Muscles Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ba975795-98dc-4cd8-8b55-3c20230d70e3.png",
+    },
+    {
+      title: "Respiratory Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/4a788b46-97f3-460e-afb6-428368f779ee.png",
+    },
+    {
+      title: "Pain Relief",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/5fa692ad-6647-4db5-9cb4-eb0bd9111331.png",
+    },
+    {
+      title: "Eye Care",
+      img: "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/631e3dca-3d5b-49f8-832b-8341ec4784e7.png",
+    },
+  ];
+  
+  export const topSlidingImage = [
+    {
+      img: "https://onemg.gumlet.io/a_ignore,w_899,h_200,c_fit,q_auto,f_auto/8e66b76c-9fd0-4215-88c4-61f1800e3847.png",
+    },
+    {
+      img: "https://onemg.gumlet.io/a_ignore,w_899,h_200,c_fit,q_auto,f_auto/6d13550f-4564-47f9-9e74-28ddcd6bda10.png",
+    },
+    {
+      img: "https://onemg.gumlet.io/a_ignore,w_899,h_200,c_fit,q_auto,f_auto/eynhkhgh6x2z3lrfibod.png",
+    },
+    {
+      img: "https://onemg.gumlet.io/a_ignore,w_899,h_200,c_fit,q_auto,f_auto/afd16962-4fde-4fd1-9c54-e8fafd6cea9d.png"
+    },
+    {
+      img: "https://onemg.gumlet.io/a_ignore,w_899,h_200,c_fit,q_auto,f_auto/04e727da-01cf-4573-b042-39fb593ea6db.png"
+    },
+    {
+      img: "https://onemg.gumlet.io/a_ignore,w_899,h_200,c_fit,q_auto,f_auto/8e66b76c-9fd0-4215-88c4-61f1800e3847.png"
+    },
+  ];
+  
+  export const topSellingProducts = [
+    {
+      link: "https://www.1mg.com/otc/sugar-free-gold-low-calorie-sweetener-otc131475",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/cropped/bpgstknem0udqzp9p4dm.jpg",
+      name: "Sugar Free Gold Low Calorie Sweetener",
+      qty: "bottle of 500 Pllets",
+      strikePrice: "285",
+      discount: "12% off",
+      price: "249",
+    },
+    {
+      link: "https://www.1mg.com/otc/so-sweet-stevia-sugar-free-tablet-otc337806",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/fud0ky8qdz1mupsc58ze.jpg",
+      name: "So Sweet Stevia Sugar Free Tablet",
+      qty: "Packet of 100 Tablets",
+      strikePrice: "120",
+      discount: "2% off",
+      price: "117",
+    },
+    {
+      link: "https://www.1mg.com/otc/so-sweet-xylitol-powder-natural-sweetener-otc337812",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/kuhv7g7ouhb8jwhvsgmk.jpg",
+      name: "So Sweet Xylitol Powder Natural Sweetener",
+      qty: "Packet of 250 gm Pouch",
+      strikePrice: "350",
+      discount: "7% off",
+      price: "324",
+    },
+    {
+      link: "https://www.1mg.com/otc/alma-combo-pack-of-sugarfree-650-tablet-and-stevia-175-tablet-low-calorie-sweetener-otc528269",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/lybhdtfz2jccygh5lgpw.jpg",
+      name: "Alma Combo Pack of Sugarfree 650 Tablet and Stevia 175 Tablet Low Calorie Sweetener",
+      qty: "Combo Pack Of 2 Bottols",
+      strikePrice: "975",
+      discount: "13% off",
+      price: "849",
+    },
+    {
+      link: "https://www.1mg.com/otc/lakanto-monkfruit-sweetener-with-erythritol-classic-white-sugar-replacement-granules-otc704818",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/jadahxgbfiupbmy8sxve.jpg",
+      name: "Lakanto Monkfruit Sweetener with Erythritol Classic White Sugar Replacement Granules",
+      qty: "Packet of 200 gm Grenules",
+      strikePrice: "525",
+      discount: "7% off",
+      price: "488",
+    },
+    {
+      link: "https://www.1mg.com/otc/sugar-free-natura-low-calorie-sweetener-pellets-otc154589",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/cropped/d8wetugtajdasve7gg0u.jpg",
+      name: "Sugar Free Natura Low Calorie Sweetener Pellets",
+      qty: "bottle of 500 Patels",
+      strikePrice: "300",
+      discount: "12% off",
+      price: "264",
+    },
+    {
+      link: "https://www.1mg.com/otc/zindagi-stevia-tablet-otc527874",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/cropped/tqblkrwqqefunrkiut4r.jpg",
+      name: "BZindagi Stevia Tablet",
+      qty: "Bottels of 100 Tablets",
+      strikePrice: "185",
+      discount: "9% off",
+      price: "149",
+    },
+    {
+      link: "https://www.1mg.com/otc/pain-relief-combo-of-dr-ortho-oil-120ml-and-organic-india-turmeric-formula-60-veg-capsule-otc614464",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/oexechydeypou9aw45xc.jpg",
+      name: "Pain Relief Combo of Dr Ortho Oil 120ml and Organic India Turmeric Formula 60 Veg Capsule",
+      qty: "Combo Pack of 2 Bottols",
+      strikePrice: "580",
+      discount: "4% off",
+      price: "564",
+    },
+    {
+      link: "https://www.1mg.com/otc/oral-care-combo-of-listerine-mouth-wash-cool-mint-500ml-and-sensodyne-repair-protect-toothpaste-100gm-otc614416",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/g6phmkjrdbw3x4nngxlr.jpg",
+      name: "Oral Care Combo of Listerine Mouth Wash Cool Mint 500ml and Sensodyne Repair & Protect Toothpaste 100gm",
+      qty: "Combo Pack Of 2 packs",
+      strikePrice: "535",
+      discount: "9% off",
+      price: "487",
+    },
+    {
+      link: "https://www.1mg.com/otc/combo-pack-of-neurobion-forte-tablet-30-evion-400mg-capsule-10-otc783417",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/irnrqy8yn0y6jmj35apg.jpg",
+      name: "Combo Pack of Neurobion Forte Tablet (30) & Evion 400mg Capsule (10)",
+      qty: "Combo Pack Of 2 Strips",
+      strikePrice: "74",
+      discount: "12% off",
+      price: "65",
+    },
+    {
+      link: "https://www.1mg.com/otc/revital-combo-pack-of-revital-h-30-capsule-and-revital-h-woman-30-tablet-otc618300",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/hkivk33molknvlppjtkg.jpg",
+      name: "Revital Combo Pack of Revital H 30 Capsule and Revital H Woman 30 Tablet",
+      qty: "Combo Pack Of 2 packs",
+      strikePrice: "760",
+      discount: "12% off",
+      price: "585",
+    },
+    {
+      link: "https://www.1mg.com/otc/bayer-combo-pack-of-supradyn-daily-multivitamin-15-tablet-saridon-10-tablet-otc681220",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/ylfuylfxrh0cvzrxopf3.jpg",
+      name: "Bayer Combo Pack of Supradyn Daily Multivitamin 15 Tablet & Saridon 10 Tablet",
+      qty: "Combo Pack Of 2 Strips",
+      strikePrice: "97",
+      discount: "7% off",
+      price: "89",
+    },
+    {
+      link: "https://www.1mg.com/otc/combo-pack-of-zincovit-tablet-limcee-chewable-tablet-orange-15-each-otc783384",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/qehyx880yecto35fwmen.jpg",
+      name: "Combo Pack of Zincovit Tablet & Limcee Chewable Tablet Orange (15 Each)",
+      qty: "Combo Pack Of 2 Strips",
+      strikePrice: "130",
+      discount: "14% off",
+      price: "112",
+    },
+    {
+      link: "https://www.1mg.com/otc/combo-pack-of-cipcal-500-tablet-15-evion-400mg-capsule-10-otc783438",
+      img: "https://onemg.gumlet.io/a_ignore,w_380,h_380,c_fit,q_auto,f_auto/ei8hx4oab3o7oqvyoozi.jpg",
+      name: "Combo Pack of Cipcal 500 Tablet (15) & Evion 400mg Capsule (10)",
+      qty: "Combo Pack Of 2 Strips",
+      strikePrice: "122",
+      discount: "24% off",
+      price: "89",
+    },
+  ];
+  
