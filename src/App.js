e286983763b10d73fb2d9eae6a13410fc1a717c3
laxmi choudhari <laxmichoudhari04@gmail.com>
2023-01-21 07:46:01 +0530

    day 4 wwork added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from './components/Footer';
+import { Navbar } from './components/Navbar';
+import Homepage from "./Pages/Homepage"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Homepage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Box, Text, Link, Image } from "@chakra-ui/react"
+import { topSlidingImage } from "../components/Constants"
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import ProductsAndBrandsBox from "../components/ProductsAndBrandsBox"
+import {
+  health,
+} from "../components/Constants";
+import BrandsCard from "../components/BrandsCard";
+import Details from "../components/Details"
+
+const Homepage = () => {
+  return (
+    <Box fontFamily="Clear Sans">
+      {/* Top carousel and image */}
+      <Box display={"flex"} border="1px solid lightgrey" p="0.5">
+        <Box w={"70%"}>
+          <AliceCarousel
+            autoPlay
+            // autoplayTimeout = "2000"
+            autoPlayInterval="2000"
+            disableButtonsControls="true"
+            disableDotsControls="true"
+
+          >
+            {topSlidingImage.map((e, i) => (
+              <Image w="100%" key={i} src={e.img} />
+            ))}
+          </AliceCarousel>
+        </Box>
+        <Box w="30%">
+          <Link
+            target="_blank"
+            href="https://www.1mg.com/labs/test/advanced-cardiac-risk-markers-34953"
+          >
+            <Image
+              h="100%"
+              src="https://onemg.gumlet.io/a_ignore,w_480,h_200,c_fit,q_auto,f_auto/85bbe9a7-230b-4836-bff3-d5f451b426a7.png"
+            />
+          </Link>
+        </Box>
+      </Box>
+
+      {/* Top Carousel and image done */}
+
+      {/* Baisic line */}
+      <Box textAlign={"center"} p="2rem" border="1px solid lightgrey">
+        <Text
+          fontWeight={"400"}
+          fontSize={"25px"}
+          lineHeight={"31px"}
+          color={"#666666"}
+        >
+          Tata 1mg: India’s Leading Online Pharmacy & Healthcare Platform
+        </Text>
+      </Box>
+
+      {/* End */}
+
+      {/* Brands and Products */}
+
+      <ProductsAndBrandsBox />
+      <BrandsCard data={health} />
+      <Details />
+      
+    </Box>
+  )
+}
+
+export default Homepage
